b37d268751ba12e62d6857511efeed288de977ec
quanwei <quanwei9958@126.com>
2016-09-25 21:47:40 +0800

    add ccount

---
+++ ccount.cc
@@ -0,0 +1,35 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int mcount=0 ;
+void ccount(vector<int>& array,vector<int>& stack,int deep)  {
+  vector<int> temp(array);
+  if(!deep) {
+    mcount++;
+    for(const int i : stack)
+      cout << i;
+    cout << endl;
+    return;
+  }
+  else{
+    for(const int i : array){
+      stack.push_back(i);
+      temp.pop_back();
+      ccount(temp,stack,deep-1);
+      stack.pop_back();
+    }
+  }
+}
+
+int main(){
+  const int N = 5;
+  int width = 2;
+  vector<int> array ;
+  vector<int> stack;
+  for(int i=0;i<N;++i) array.push_back(i);
+  ccount(array,stack,width);
+  cout << "count is" << mcount <<endl;
+  return 0;
+}
