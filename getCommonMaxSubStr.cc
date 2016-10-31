b6c5a1b983fb74e6cd66f90a0ee32e11d351f1f8
quanwei <quanwei9958@126.com>
2016-10-31 12:56:05 +0800

    refactor getCommonMaxSubStr

--- getCommonMaxSubStr.cc
+++ getCommonMaxSubStr.cc
@@ -1,39 +1,46 @@
-char *getCommonMaxSubStr(char *ptr1, char *ptr2, int *getlen)
-{
-	int len = strlen(ptr1);
-	int *parr = (int *)malloc(sizeof(int)*len);
-	for (int i = 0; i < len; ++i)
-	{
-		int max = 0;	
-		for (int j = 0; j < (int)strlen(ptr2); ++j)
-		{
-			int si = i;
-			int sj = j;
-			int num = 0;
-			while (*(ptr1 + si) == *(ptr2 + sj) && *(ptr2+sj)!='\0')
-			{
-				++si;
-				++sj;
-				++num;
-			}
-			if (num > max)
-				max = num;
-		}
-		parr[i] = max;
-	}
+/*
+  需使用c++11进行编译
+*/
+# include <iostream>
+# include <string>
+# include <cstring>
 
-	int sublen = 0;
-	int *ptr = NULL;
-	for (int i = 0; i < len; ++i)
-	{
-		if (parr[i]>sublen)
-		{
-			sublen = parr[i];
-			ptr = &parr[i];
-		}
-	}
-	*getlen = sublen;
-	len = ptr - parr;
-	free(parr);
-	return ptr1 +len;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct commonStr{
+  const char* ptr;
+  int len;
+};
+
+/*
+  获取最大公共子串
+  return: commonStr
+*/
+struct commonStr getCommonMaxSubStr(const char* ptr1,const int len1,const char* ptr2,const int len2){
+  if(len1 ==0 || len2==0) return { 0,0 };
+  //optimize
+  if(len2 > len1){
+    return getCommonMaxSubStr(ptr2,len2,ptr1,len1);
+  }
+  //find
+  const char* ptr = strstr(ptr1,string(ptr2,len2).c_str());
+  if(ptr) return {ptr,len2};
+  
+  //compare
+  auto ans1 = getCommonMaxSubStr(ptr1,len1,ptr2+1,len2-1);
+  auto ans2 = getCommonMaxSubStr(ptr1,len1,ptr2,len2-1);
+
+  //return
+  return (ans1.len > ans2.len ? ans1 : ans2);
+}
+
+int main(){
+  string a1="hdsallellohsadllx";
+  string a2="llox";
+
+  auto a = getCommonMaxSubStr(a1.c_str(),a1.length(),a2.c_str(),a2.length());
+  cout << a.len <<": "<<string(a.ptr,a.len)<<endl;
+  return 0;
 }
